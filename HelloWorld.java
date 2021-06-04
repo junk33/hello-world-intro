37c5ea63b27ce5712f3f3d94656b46600a1f533a
Yusuf Pisan <pisan@uw.edu>
2021-06-04 11:05:46 -0700

    run workflow

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,21 +1,19 @@
 // Your First Program
 
-
-
 class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
-    
+
     public static void printStarPiramid() {
-        System.out.println("    *"); //an extra space was added in the end to make test case failed
+        System.out.println("    *"); // an extra space was added in the end to make test case failed
         System.out.println("   ***");
         System.out.println("  *****");
         System.out.println(" *******");
         System.out.println("*********");
     }
-    
+
     public static void printFace() {
         System.out.println(" +\"\"\"\"\"+\"); //an extra space was added in the end to make test case failed
         System.out.println("[| o o |]");
@@ -23,14 +21,16 @@ class HelloWorld {
         System.out.println(" | '-' | ");
         System.out.println(" +-----+ ");
     }
-    
-    public static void printLongSentence() 
-    {
-        System.out.println("This is a very long sentence that contains over 100 characters, and it should cause a warming in checkstyle!");
+
+    public static void printLongSentence() {
+        System.out.println(
+                "This is a very long sentence that contains over 100 characters, and it should cause a warming in checkstyle!");
     }
-    
-    public static void nothing(){
-        if (true) if (false) 
-            for (int i=0; i<10; i++) i++;
+
+    public static void nothing() {
+        if (true)
+            if (false)
+                for (int i = 0; i < 10; i++)
+                    i++;
     }
 }
