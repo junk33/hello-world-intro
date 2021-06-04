4c38702973d451857e54be3a9a4a05172e05e7ea
Yusuf Pisan <pisan@uw.edu>
2021-06-04 11:45:36 -0700

    tester

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@
 class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello, WWWWorld!");
+        System.out.println("Hello, WWWWWorld!");
     }
 
     public static void printStarPiramid() {
