f3916ab4beaca62d43184804643fc489cef22596
Yusuf Pisan <pisan@uw.edu>
2021-06-04 18:11:19 -0700

    w47

--- .github/workflows/build-with-maven.sh
+++ .github/workflows/build-with-maven.sh
@@ -5,4 +5,6 @@ echo "Running build-with-maven.sh script"
 git clone https://github.com/junk33/hello-world-test.git
 cp HelloWorld.java hello-world-test/src/main/java
 cd hello-world-test
-mvn clean compile test --quiet --no-transfer-progress --batch-mode --fail-at-end
+# --fail-never
+# --fail-at-end
+mvn clean compile test --quiet --no-transfer-progress --batch-mode --fail-never
--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@
 class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello, WWWW46orld!");
+        System.out.println("Hello, WWWW47orld!");
     }
 
     public static void printStarPiramid() {
