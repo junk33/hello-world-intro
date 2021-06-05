40192ff86919ff5b5d935f7152a02b7962794cef
Yusuf Pisan <pisan@uw.edu>
2021-06-04 18:23:45 -0700

    w50

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@
 class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello, WWWW49orld!");
+        System.out.println("Hello, WWWW50orld!");
     }
 
     public static void printStarPiramid() {
